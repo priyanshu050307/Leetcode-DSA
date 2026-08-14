d43e8bbdf0619e059aef1c2acb7e3f77c7a8d690
Priyanshu Vishwakarma <priyanshu.vish2007@gmail.com>
2026-08-14 10:01:58 +0530

    Time: 14 ms (12.08%), Space: 13.3 MB (28.12%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -4,28 +4,22 @@ public:
         int left = 0;
         int count = 0;
         int ans = 0;
-
         for (int right = 0; right < s.size(); right++) {
-
             if (s[right] == 'a' || s[right] == 'e' || 
                 s[right] == 'i' || s[right] == 'o' || 
                 s[right] == 'u') {
                 count++;
             }
-
-            while (right - left + 1 > k) {
+            if (right - left + 1 > k) {
                 if (s[left] == 'a' || s[left] == 'e' || 
                     s[left] == 'i' || s[left] == 'o' || 
                     s[left] == 'u') {
                     count--;
                 }
-
                 left++;
             }
-
             ans = max(ans, count);
         }
-
         return ans;
     }
 };
